6c370697e9116dda21e66971022c51d203deb5e9
Jason Yundt <swagfortress@gmail.com>
2021-11-08 14:55:46 -0500

    Add mouse controls

--- project/scenes_and_scripts/paddle.gd
+++ project/scenes_and_scripts/paddle.gd
@@ -5,7 +5,29 @@ extends Node2D
 
 
 const MAX_SPEED := 500  # Unit: px/sec
+const MOUSE_CONTROL_DISABLING_ACTIONS := ["ui_left", "ui_right"]
+onready var mouse_controls := false
 
 
-func _physics_process(delta):
-	position.x += Input.get_axis("ui_left", "ui_right") * MAX_SPEED * delta
+func _physics_process(delta: float) -> void:
+	var max_distance_this_tic: float = MAX_SPEED * delta  # Unit: px
+	if mouse_controls:
+		var distance_to_mouse: float = get_local_mouse_position().x
+		if abs(distance_to_mouse) <= max_distance_this_tic:
+			global_position.x = get_global_mouse_position().x
+		elif distance_to_mouse < 0:
+			position.x -= max_distance_this_tic
+		else:
+			position.x += max_distance_this_tic
+	else:
+		position.x += Input.get_axis("ui_left", "ui_right") * max_distance_this_tic
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("enable_mouse_controls"):
+		mouse_controls = true
+	else:
+		for action in MOUSE_CONTROL_DISABLING_ACTIONS:
+			if event.is_action_pressed(action):
+				mouse_controls = false
+				return
