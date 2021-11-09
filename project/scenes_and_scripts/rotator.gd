8ceb6433e08f5bf06edf9891a48c4e62f766dc2a
Jason Yundt <swagfortress@gmail.com>
2021-11-09 09:16:29 -0500

    Create first level

---
+++ project/scenes_and_scripts/rotator.gd
@@ -0,0 +1,10 @@
+# SPDX-FileNotice: 🅭🄍1.0 This file was dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
+# SPDX-FileContributor: Jason Yundt <swagfortress@gmail.com> (2021)
+# SPDX-FileType: SOURCE
+extends Node2D
+
+
+const SPEED: float = deg2rad(1)  # Unit: rad/sec
+
+func _physics_process(delta: float) -> void:
+	rotate(SPEED * delta)
